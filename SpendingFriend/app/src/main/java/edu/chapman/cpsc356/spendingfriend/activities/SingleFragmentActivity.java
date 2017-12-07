2fb06806d1a04300e580300172a18eec75e16cab
upegu100 <upegu100@mail.chapman.edu>
2017-12-06 23:39:35 -0800

    Added SingleFragmentActivity and activity_single_fragment.xml

---
+++ SpendingFriend/app/src/main/java/edu/chapman/cpsc356/spendingfriend/activities/SingleFragmentActivity.java
@@ -0,0 +1,31 @@
+package edu.chapman.cpsc356.spendingfriend.activities;
+
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import edu.chapman.cpsc356.spendingfriend.R;
+
+public abstract class SingleFragmentActivity extends AppCompatActivity {
+
+    /*getFragment() method*/
+    protected abstract Fragment getFragment();
+
+    /*onCreate() method*/
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_single_fragment);
+
+        showFragment(getFragment());
+    }
+
+    /*showFragment method*/
+    private void showFragment(Fragment frag)
+    {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fl_fragment_container, frag)
+                .commit();
+    }
+}
