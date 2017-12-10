73105e587dd845d2fb1102008fce7fa3e9206238
upegu100 <upegu100@mail.chapman.edu>
2017-12-10 00:51:16 -0800

    Created BudgetActivity and BudgetFragment

---
+++ SpendingFriend/app/src/main/java/edu/chapman/cpsc356/spendingfriend/activities/BudgetActivity.java
@@ -0,0 +1,18 @@
+package edu.chapman.cpsc356.spendingfriend.activities;
+
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import edu.chapman.cpsc356.spendingfriend.fragments.BudgetFragment;
+
+public class BudgetActivity extends SingleFragmentActivity {
+
+    private BudgetFragment budgetFragment;
+    @Override
+    protected Fragment getFragment()
+    {
+        this.budgetFragment = new BudgetFragment();
+        return this.budgetFragment;
+    }
+}
--- SpendingFriend/app/src/main/java/edu/chapman/cpsc356/spendingfriend/activities/MainActivity.java
+++ SpendingFriend/app/src/main/java/edu/chapman/cpsc356/spendingfriend/activities/MainActivity.java
@@ -1,5 +1,7 @@
 package edu.chapman.cpsc356.spendingfriend.activities;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -30,9 +32,12 @@ public class MainActivity extends AppCompatActivity {
 
     public void onBudgetClick(View view)
     {
+        Intent intent = new Intent(this, BudgetActivity.class);
+        startActivity(intent);
     }
 
     public void onSummaryClick(View view)
     {
     }
+
 }
