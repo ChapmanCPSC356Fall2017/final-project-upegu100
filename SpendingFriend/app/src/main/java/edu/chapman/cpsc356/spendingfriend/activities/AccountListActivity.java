7a065ef4349b62227b0c1474eb93180b85261593
upegu100 <upegu100@mail.chapman.edu>
2017-12-10 19:25:20 -0800

    Added DateDialogFragment and menu layouts for Account and Transaction

--- SpendingFriend/app/src/main/java/edu/chapman/cpsc356/spendingfriend/activities/AccountListActivity.java
+++ SpendingFriend/app/src/main/java/edu/chapman/cpsc356/spendingfriend/activities/AccountListActivity.java
@@ -1,6 +1,8 @@
 package edu.chapman.cpsc356.spendingfriend.activities;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 import edu.chapman.cpsc356.spendingfriend.R;
 import edu.chapman.cpsc356.spendingfriend.fragments.AccountFragment;
@@ -15,5 +17,13 @@ public class AccountListActivity extends SingleFragmentActivity {
     {
         this.accountListFragment = new AccountListFragment();
         return this.accountListFragment;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.activity_account_list, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
--- SpendingFriend/app/src/main/java/edu/chapman/cpsc356/spendingfriend/activities/TransactionListActivity.java
+++ SpendingFriend/app/src/main/java/edu/chapman/cpsc356/spendingfriend/activities/TransactionListActivity.java
@@ -3,6 +3,8 @@ package edu.chapman.cpsc356.spendingfriend.activities;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 import edu.chapman.cpsc356.spendingfriend.R;
 import edu.chapman.cpsc356.spendingfriend.fragments.TransactionListFragment;
@@ -16,5 +18,13 @@ public class TransactionListActivity extends SingleFragmentActivity
     {
         this.transactionListFragment = new TransactionListFragment();
         return this.transactionListFragment;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.activity_transaction_list, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
