dc28abe02b813f259a5f51f9a9485f5d8d36f8fc
upegu100 <upegu100@mail.chapman.edu>
2017-12-10 19:32:29 -0800

    Added functionality to menu_add_account item

--- SpendingFriend/app/src/main/java/edu/chapman/cpsc356/spendingfriend/activities/AccountListActivity.java
+++ SpendingFriend/app/src/main/java/edu/chapman/cpsc356/spendingfriend/activities/AccountListActivity.java
@@ -1,12 +1,16 @@
 package edu.chapman.cpsc356.spendingfriend.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import edu.chapman.cpsc356.spendingfriend.R;
+import edu.chapman.cpsc356.spendingfriend.collections.AccountCollection;
 import edu.chapman.cpsc356.spendingfriend.fragments.AccountFragment;
 import edu.chapman.cpsc356.spendingfriend.fragments.AccountListFragment;
+import edu.chapman.cpsc356.spendingfriend.models.AccountModel;
 
 public class AccountListActivity extends SingleFragmentActivity {
 
@@ -25,5 +29,25 @@ public class AccountListActivity extends SingleFragmentActivity {
         MenuInflater menuInflater = getMenuInflater();
         menuInflater.inflate(R.menu.activity_account_list, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch (item.getItemId())
+        {
+            case R.id.menu_add_account:
+                AccountModel newAccount = new AccountModel();
+                AccountCollection.GetInstance().getAccounts().add(0, newAccount);
+
+                Intent intent = new Intent(this, AccountActivity.class);
+                intent.putExtra(AccountActivity.EXTRA_ACCOUNT_NUMBER, newAccount.getNumber());
+                startActivity(intent);
+
+                return true;
+
+            default:
+                return false;
+        }
     }
 }
