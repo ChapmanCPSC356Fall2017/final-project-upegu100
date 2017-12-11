cd2f23c89f9686378911ee1adee8e1c87bdb1192
upegu100 <upegu100@mail.chapman.edu>
2017-12-10 20:00:06 -0800

    Added functionality for menu_add_transaction item

--- SpendingFriend/app/src/main/java/edu/chapman/cpsc356/spendingfriend/activities/TransactionListActivity.java
+++ SpendingFriend/app/src/main/java/edu/chapman/cpsc356/spendingfriend/activities/TransactionListActivity.java
@@ -1,13 +1,17 @@
 package edu.chapman.cpsc356.spendingfriend.activities;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import edu.chapman.cpsc356.spendingfriend.R;
+import edu.chapman.cpsc356.spendingfriend.collections.TransactionCollection;
 import edu.chapman.cpsc356.spendingfriend.fragments.TransactionListFragment;
+import edu.chapman.cpsc356.spendingfriend.models.TransactionModel;
 
 public class TransactionListActivity extends SingleFragmentActivity
 {
@@ -26,5 +30,25 @@ public class TransactionListActivity extends SingleFragmentActivity
         MenuInflater menuInflater = getMenuInflater();
         menuInflater.inflate(R.menu.activity_transaction_list, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch (item.getItemId())
+        {
+            case R.id.menu_add_transaction:
+                TransactionModel newTransaction = new TransactionModel();
+                TransactionCollection.GetInstance().getTransactions().add(newTransaction);
+
+                Intent intent = new Intent(this, TransactionActivity.class);
+                intent.putExtra(TransactionActivity.EXTRA_TRANSACTION_ID, newTransaction.getId());
+                startActivity(intent);
+
+                return true;
+
+            default:
+                return false;
+        }
     }
 }
