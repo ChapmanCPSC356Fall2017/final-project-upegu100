7b33498c3f838a46e181a616de96e0b72f35ccd9
upegu100 <upegu100@mail.chapman.edu>
2017-12-15 23:16:16 -0800

    All done!

--- SpendingFriend/app/src/main/java/edu/chapman/cpsc356/spendingfriend/activities/MainActivity.java
+++ SpendingFriend/app/src/main/java/edu/chapman/cpsc356/spendingfriend/activities/MainActivity.java
@@ -1,7 +1,5 @@
 package edu.chapman.cpsc356.spendingfriend.activities;
 
-import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
