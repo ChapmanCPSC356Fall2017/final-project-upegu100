26ea53202b459295fb02b7b0ccc1654c7bfa8013
upegu100 <upegu100@mail.chapman.edu>
2017-12-15 23:13:53 -0800

    Added last bits of generating fake data

--- SpendingFriend/app/src/main/java/edu/chapman/cpsc356/spendingfriend/collections/TransactionCollection.java
+++ SpendingFriend/app/src/main/java/edu/chapman/cpsc356/spendingfriend/collections/TransactionCollection.java
@@ -50,16 +50,23 @@ public class TransactionCollection
         return null;
     }
 
+    //Generates Fake Transactions
     private void generateFakeData()
     {
         //Fake Withdrawals
         String[] withdrawalNames = {"New Years Party Favors", "Romantic Dinner", "Green Decorations",
                 "Egg Dye", "Mother's Day Gift", "Father's Day Brunch", "American Flag",
-                "Barbeque from Home Depot", "School Supplies", "Pumpkin", "Turkey", "Christmas Gift"};
-        double[] withdrawalAmounts = {10,20,30,40,50,60,70,80,90,100,110,120};
-        String[] withdrawalAccountNames = {"Cash", "BOA Checking", "Key Bank Savings", "Cash",
+                "Barbeque from Home Depot", "School Supplies", "Pumpkin", "Turkey", "Christmas Gift",
+        "Withdrawal 1", "Withdrawal 2", "Withdrawal 3", "Withdrawal 4", "Withdrawal 5", "Withdrawal 6",
+                "Withdrawal 7", "Withdrawal 8", "Withdrawal 9", "Withdrawal 10", "Withdrawal 11",
+                "Withdrawal 12"};
+        double[] withdrawalAmounts = {10,20,30,40,50,60,70,80,90,100,110,120,10,20,30,40,50,60,70,80,
+                90,100,110,120};
+        String[] withdrawalAccountNames = {"Key Bank Savings", "BOA Checking", "Cash", "Cash",
                 "BOA Checking", "Key Bank Savings", "Cash", "BOA Checking", "Key Bank Savings", "Cash",
-                "BOA Checking", "Key Bank Savings"};
+                "BOA Checking", "Key Bank Savings", "Cash", "Cash", "Cash", "Cash", "BOA Checking",
+                "BOA Checking", "BOA Checking", "BOA Checking", "Key Bank Savings", "Key Bank Savings",
+                "Key Bank Savings", "Key Bank Savings"};
         DateTime[] withdrawalDates = {
                 new DateTime(2017,1,1,0,0),
                 new DateTime(2017,2,1,0,0),
@@ -72,17 +79,33 @@ public class TransactionCollection
                 new DateTime(2017,9,1,0,0),
                 new DateTime(2017,10,1,0,0),
                 new DateTime(2017,11,1,0,0),
-                new DateTime(2017,12,1,0,0)};
+                new DateTime(2017,12,1,0,0),
+                new DateTime(2017,1,5,0,0),
+                new DateTime(2017,2,5,0,0),
+                new DateTime(2017,3,5,0,0),
+                new DateTime(2017,4,5,0,0),
+                new DateTime(2017,5,5,0,0),
+                new DateTime(2017,6,5,0,0),
+                new DateTime(2017,7,5,0,0),
+                new DateTime(2017,8,5,0,0),
+                new DateTime(2017,9,5,0,0),
+                new DateTime(2017,10,5,0,0),
+                new DateTime(2017,11,5,0,0),
+                new DateTime(2017,12,5,0,0)};
 
         //Fake Deposits
         String[] depositNames = {"January Work Payment", "February Work Payment", "March Work Payment",
                 "April Work Payment", "May Work Payment", "June Work Payment", "July Work Payment",
                 "August Work Payment", "September Work Payment", "October Work Payment", "November Work Payment",
-                "December Work Payment"};
-        double[] depositAmounts = {100,200,300,400,500,600,700,800,900,1000,1100,1200};
-        String[] depositAccountNames = {"Cash", "BOA Checking", "Key Bank Savings", "Cash",
+                "December Work Payment", "Deposit 1", "Deposit 2", "Deposit 3", "Deposit 4", "Deposit 5",
+                "Deposit 6", "Deposit 7", "Deposit 8", "Deposit 9","Deposit 10", "Deposit 11","Deposit 12"};
+        double[] depositAmounts = {100,200,300,400,500,600,700,800,900,1000,1100,1200, 100,200,300,
+                400,500,600,700,800,900,1000,1100,1200};
+        String[] depositAccountNames = {"Key Bank Savings", "BOA Checking", "Cash", "Cash",
                 "BOA Checking", "Key Bank Savings", "Cash", "BOA Checking", "Key Bank Savings", "Cash",
-                "BOA Checking", "Key Bank Savings"};
+                "BOA Checking", "Key Bank Savings", "Cash", "Cash", "Cash", "Cash", "BOA Checking",
+                "BOA Checking", "BOA Checking", "BOA Checking", "Key Bank Savings", "Key Bank Savings",
+                "Key Bank Savings", "Key Bank Savings"};
         DateTime[] depositDates = {
                 new DateTime(2017,1,15,0,0),
                 new DateTime(2017,2,15,0,0),
@@ -95,18 +118,25 @@ public class TransactionCollection
                 new DateTime(2017,9,15,0,0),
                 new DateTime(2017,10,15,0,0),
                 new DateTime(2017,11,15,0,0),
-                new DateTime(2017,12,15,0,0)};
-
+                new DateTime(2017,12,15,0,0),
+                new DateTime(2017,1,25,0,0),
+                new DateTime(2017,2,25,0,0),
+                new DateTime(2017,3,25,0,0),
+                new DateTime(2017,4,25,0,0),
+                new DateTime(2017,5,25,0,0),
+                new DateTime(2017,6,25,0,0),
+                new DateTime(2017,7,25,0,0),
+                new DateTime(2017,8,25,0,0),
+                new DateTime(2017,9,25,0,0),
+                new DateTime(2017,10,25,0,0),
+                new DateTime(2017,11,25,0,0),
+                new DateTime(2017,12,25,0,0)};
 
         for (int i = 0; i < withdrawalNames.length; ++i)
         {
             transactions.add(new TransactionModel(withdrawalNames[i], withdrawalAmounts[i], false,
                     AccountCollection.GetInstance().getAccountByName(withdrawalAccountNames[i]),
                     withdrawalDates[i]));
-        }
-
-        for (int i =0 ; i < depositNames.length; ++i)
-        {
             transactions.add(new TransactionModel(depositNames[i], depositAmounts[i], true,
                     AccountCollection.GetInstance().getAccountByName(depositAccountNames[i]),
                     depositDates[i]));
